fa341ee2f7e7c75e295de26e921d3c363f0f8439
Gergely Nagy <ngg@ngg.hu>
2010-12-19 10:07:37 +0000

    bugfixes
    git-svn-id: http://groupext.googlecode.com/svn/trunk@5 fda7ef6f-4cb4-7828-2ba4-fdeeaeeecda9

--- GroupExt/GroupExt/ConjugacyClasses.m
+++ GroupExt/GroupExt/ConjugacyClasses.m
@@ -55,22 +55,4 @@ GroupConjugacyClassNum[g_, a_] := Module[{classes},
 GroupConjugacyClassRepresentative[g_, a_] := GroupConjugacyClassRepresentatives[g][[GroupConjugacyClassNum[g, a]]]
 	
 GroupConjugacyClassInverses[g_] := Map[GroupConjugacyClassNum[g, #^(-1)]&, GroupConjugacyClassRepresentatives[g]] 	
-	
-GroupConjugacyClassProductTable[g_] := GroupConjugacyClassProductTable[g] = Module[{repr, i, j, r},
-	repr = GroupConjugacyClassRepresentatives[g];
-	r = GroupNumOfConjugacyClasses[g];
-	Table[GroupConjugacyClassNum[g, repr[[i]]**repr[[j]]], {i, r}, {j, r}]
-]
 
-GroupConjugacyClassPower[g_, a_, i_] := Module[{prod, ret, x, j},
-	prod = GroupConjugacyClassProductTable[g];
-	ret = 1;
-	x = a;
-	j = i;
-	While[j > 0,
-		If[Mod[j, 2] == 1, ret = prod[[ret, x]]];
-		j = Floor[j/2];
-		x = prod[[x, x]]	
-	];
-	ret
-]
